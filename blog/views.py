e27d576888fe272a9e98673486c69d72a8942409
Abdur Razzak <73746963+abdur-razzak2672@users.noreply.github.com>
2021-11-07 02:15:15 +0600

    Update code

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import blog
 
-# Create your views here.
+def all_blogs(request):
+
+    blogs = blog.objects.all()
+    return render(request , 'blog/all_blogs.html',{'blogs':blogs})
+    
