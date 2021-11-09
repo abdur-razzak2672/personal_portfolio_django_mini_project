cdf58891b59327752575fcc2d67223d79d50da40
Abdur Razzak <73746963+abdur-razzak2672@users.noreply.github.com>
2021-11-10 00:00:59 +0600

    update code

--- blog/urls.py
+++ blog/urls.py
@@ -2,7 +2,9 @@
 from django.urls import path
 
 from . import views
+app_name = 'blog'
 
 urlpatterns = [
     path('',views.all_blogs , name = 'all_blogs'),
+    path('<int:blog_id>/',views.blog_detail ,name = 'blog_detail')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render , get_object_or_404
 from .models import blog
 
 def all_blogs(request):
 
-    blogs = blog.objects.all()
+    blogs = blog.objects.order_by('-date')[:5]
     return render(request , 'blog/all_blogs.html',{'blogs':blogs})
-    
+
+
+def blog_detail(request, blog_id) :
+    all_blog = get_object_or_404(blog , pk=blog_id)
+    return render(request,'blog/blog_details.html',{'all_blog':all_blog})
