65ee4aa3d73a8eaeeab214f4652b52fdaf7d0420
Abdur Razzak <73746963+abdur-razzak2672@users.noreply.github.com>
2021-11-12 01:48:58 +0600

    updated

--- personal_portfolio/urls.py
+++ personal_portfolio/urls.py
@@ -22,7 +22,9 @@ from portfolio import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.home , name = 'home'),
-    path('blog/', include('blog.urls'))
+    path('about/',views.about , name = 'about'),
+    path('blogs/', include('blog.urls')),
+    path('teams',views.team , name = 'team')
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- portfolio/views.py
+++ portfolio/views.py
@@ -4,3 +4,10 @@ from .models import project
 def home(request) :
     projects = project.objects.all()
     return render(request , 'portfolio/home.html', {'projects':projects})
+
+def about(request):
+    return render(request,'portfolio/about.html')
+
+def team(request):
+    projects = project.objects.all()
+    return render(request,'portfolio/team.html', {'projects':projects})
